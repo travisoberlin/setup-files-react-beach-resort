5b7d56ebc6d5590b019fb5d76a3326d5d6a4e8bb
Travis Oberlin <travis.oberlin@gmail.com>
2019-12-15 21:24:55 -0500

    initial
    initial commit

---
+++ .history/resortproject/src/App_20191215153039.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  hello from App
+    </div>
+  );
+}
+
+export default App;
---
+++ .history/resortproject/src/App_20191215153314.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+  hello from App
+    </div>
+  );
+}
+
+export default App;
